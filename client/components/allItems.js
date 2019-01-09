b4fb52be8ea7f5916c785cd5dddb806b965d10f0
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-09 17:11:51 -0500

    completed all items component, integrated redux (shows all products from database), attempted enzyme test but couldn’t figure it out

--- client/components/allItems.js
+++ client/components/allItems.js
@@ -4,15 +4,32 @@ import {connect} from 'react-redux'
 import {fetchAllItems} from '../store/item'
 
 class AllItems extends Component {
-  componentDidMount() {}
+  async componentDidMount() {
+    await this.props.fetchAllItems()
+  }
 
   render() {
-    return <div>all games view</div>
+    const allGames = this.props.allItems
+
+    return (
+      <div>
+        {allGames.map(game => {
+          return (
+            <div className="game" key={game.id}>
+              <img src={game.imageUrl} />
+              <h3>{game.name}</h3>
+              {/* change this to decimal!!! */}
+              <p>CHANGE ME TO DECIMAL{game.price}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
   }
 }
 
 const mapStateToProps = state => ({
-  allItems: state.allItems
+  allItems: state.item.allItems
 })
 
 const mapDispatchToProps = dispatch => ({
---
+++ client/components/allItems.spec.js
@@ -0,0 +1,55 @@
+/* global describe beforeEach it */
+
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+// import enzyme, {mount} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import {AllItems} from './allItems'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('AllItems', () => {
+  const fakeItem = [
+    {
+      id: 1,
+      imageUrl: 'https://place-hold.it/300',
+      name: 'Uno',
+      price: 10.28
+    }
+  ]
+
+  const fakeItems = [
+    {
+      id: 1,
+      imageUrl: 'https://place-hold.it/300',
+      name: 'Uno',
+      price: 10.28
+    },
+    {
+      id: 2,
+      imageUrl: 'https://place-hold.it/300',
+      name: 'Avalon',
+      price: 19.99
+    },
+    {
+      id: 3,
+      imageUrl: 'https://place-hold.it/300',
+      name: 'Risk',
+      price: 19.97
+    }
+  ]
+
+  beforeEach(() => {
+    let wrapper = shallow(<AllItems allItems={fakeItem} />)
+    // let wrapper = mount(<AllItems allItems={fakeItems} />)
+  })
+
+  // it('renders the item name in an h3', () => {
+  //   console.log(wrapper)
+  //   // expect(wrapper.find('ul')).to.have.length(1)
+  //   expect(allItems.find('game').to.have.lengthOf(3)
+  //   // expect(wrapper[0].find('h3').text()).to.be.equal('Uno')
+  // })
+})
