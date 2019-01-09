1131991cd5514c8a280803919d5550cc2eef8610
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-09 17:23:55 -0500

    trying to sort out allItems

--- client/components/AllItems.js
+++ client/components/AllItems.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {fetchAllItems} from '../store/item'
 
-const AllItems = () => {
-  return <h1>all items</h1>
+class AllItems extends Component {
+  async componentDidMount() {
+    await this.props.fetchAllItems()
+  }
+
+  render() {
+    const allGames = this.props.allItems
+
+    return (
+      <div>
+        {allGames.map(game => {
+          return (
+            <div className="game" key={game.id}>
+              <img src={game.imageUrl} />
+              <h3>{game.name}</h3>
+              {/* change this to decimal!!! */}
+              <p>CHANGE ME TO DECIMAL{game.price}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-export default AllItems
+const mapStateToProps = state => ({
+  allItems: state.item.allItems
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchAllItems: () => dispatch(fetchAllItems())
+})
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(AllItems)
+)
