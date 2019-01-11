7325453623a25d74569af72413e338f5064ad79b
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-01-11 18:14:58 -0500

    griddddd

--- client/components/ListItems.js
+++ client/components/ListItems.js
@@ -7,16 +7,27 @@ import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
+import {withStyles} from '@material-ui/core/styles'
+
+const styles = theme => ({
+  imgPadding: {
+    maxwidth: 100,
+    // overflow: 'hidden',
+    padding: 1
+  }
+})
 
 const ListItems = props => {
   const {game} = props
+  const {classes} = props
 
   return (
     <div className="game">
       <Link to={`/game/${game.id}`}>
-        <Card m="1">
+        <Card>
           <CardActionArea>
             <CardMedia
+              className={classes.imgPadding}
               component="img"
               alt={game.name}
               height="300"
@@ -42,4 +53,6 @@ const ListItems = props => {
   )
 }
 
-export default ListItems
+export default withStyles(styles)(ListItems)
+
+// export default ListItems
