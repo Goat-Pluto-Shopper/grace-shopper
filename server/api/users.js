cc0105edf17174b029f2f8fa2b65a5d65b0b13ae
ryuash <ryuash123@gmail.com>
2019-01-15 11:14:53 -0500

    took out redirect

--- server/api/users.js
+++ server/api/users.js
@@ -5,7 +5,7 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     //because we have no admin i did it this way
-    res.status(403).redirect('/')
+    res.status(403)
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
