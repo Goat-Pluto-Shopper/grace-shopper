c75c388fb4f3d61a6694cf4104d0118b5370a0df
Marie Carter <marievcarter@gmail.com>
2019-01-14 20:18:55 -0500

    Trying to remove quantity field from item model.

--- server/db/models/orderedItems.js
+++ server/db/models/orderedItems.js
@@ -6,7 +6,7 @@ const OrderedItems = db.define('orderedItems', {
     type: Sequelize.INTEGER,
     allowNull: true
   },
-  itemQuantity: {
+  quantity: {
     type: Sequelize.INTEGER,
     defaultValue: 1
   }
