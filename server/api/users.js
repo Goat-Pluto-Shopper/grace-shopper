acf63b8da2e969e6c97a00d29d0766271c26c0d9
ryuash <ryuash123@gmail.com>
2019-01-15 10:30:00 -0500

    testing user without env

--- server/api/users.js
+++ server/api/users.js
@@ -5,17 +5,14 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     //because we have no admin i did it this way
-    if (req.user === process.env.ADMIN_SECRET) {
-      const users = await User.findAll({
-        // explicitly select only the id and email fields - even though
-        // users' passwords are encrypted, it won't help if we just
-        // send everything to anyone who asks!
-        attributes: ['id', 'email']
-      })
-      res.json(users)
-    } else {
-      res.status(403).end()
-    }
+    res.status(403).end()
+    const users = await User.findAll({
+      // explicitly select only the id and email fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+      attributes: ['id', 'email']
+    })
+    // res.json(users)
   } catch (err) {
     next(err)
   }
