3cd220d6ed0db087b0cf10af7694a87ee26fd9d0
Marie Carter <marievcarter@gmail.com>
2019-01-15 22:21:02 -0500

    Clean up console logs and unused imports.

--- client/components/ListItems.js
+++ client/components/ListItems.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom'
 // material ui imports
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
